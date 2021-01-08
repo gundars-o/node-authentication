144e0bdc590d103bff4c6d049af4c487683c2c03
gundars-o <gundars.ozolins75@gmail.com>
2021-01-08 14:23:43 +0200

    Parse and post submitted data

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require( "express" );
+var bodyParser = require( "body-parser" );
 var app = express();
 app.set( "view engine", "jade" );
+app.use( bodyParser.urlencoded( { extended: true } ) );
 app.get( '/', function( req, res ) {
     res.render( "index.jade" );
 } );
 app.get( '/register', function( req, res ) {
     res.render( "register.jade" );
+} );
+app.post( '/register', function( req, res ) {
+    res.render( res.json( req.body ) );
 } );
 app.get( '/login', function( req, res ) {
     res.render( "login.jade" );
